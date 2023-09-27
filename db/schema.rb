4ed74db4939c1059cf60b5609eae58e391f2a991
Wycliffe Yegon <95078264+captainwycliffe@users.noreply.github.com>
2023-09-27 23:31:19 +0300

    Add files via upload

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_181051) do
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
